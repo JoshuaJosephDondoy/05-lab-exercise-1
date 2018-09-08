2887aad8fed2751ac0e87a0f644caa55aa0fdad0
Joshua Joseph Dondoy <joshuadondoyq2@gmail.com>
2018-09-08 17:23:50 +0800

    My Commit Message

---
+++ app/src/main/java/com/dondoy/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.dondoy.myapplication;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClick(View view){
+        Toast toast = Toast.makeText(this, "The Record is submitted", Toast.LENGTH_LONG);
+        toast.show();
+    }
+}
